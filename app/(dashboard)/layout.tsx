ba51efb31e6d561d0631708ac7d19276f1870bd3
sha345trip <114864315+sha345trip@users.noreply.github.com>
2024-09-02 16:36:56 +0530

    first commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import LeftSideBar from "@/components/layout/LeftSideBar";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import TopBar from "@/components/layout/TopBar";
+import { ToasterProvider } from "@/lib/ToasterProvider";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "All_blueee-admin",
+  description: "A dashboard to manage all_blueee data",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+            <ToasterProvider/>
+        <div className="flex max-lg:flex-col text-grey-1">
+            <LeftSideBar />
+            <TopBar/>
+            <div className="flex-1">{children}</div>
+          </div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <LeftSideBar/>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
